a3b1647d7a614e4686647950a096e745270a64c2
Liam Jardine <jardine@chalmers.se>
2023-12-06 15:47:59 +0100

    Music is playing...

--- proj2/ssy130-files-dspkit-main/src/lab_lms.c
+++ proj2/ssy130-files-dspkit-main/src/lab_lms.c
@@ -426,18 +426,15 @@ void my_lms(float const * y, float const * x, float * xhat, float * e, int block
 	 *
 	 * doing block_size lms update iterations, i.e. something like:
 	 */
-	 int n;
-	 for (n = 0; n<block_size; n++){ 		   		//Which order should we loop n over? [0, 1, 2, ..., block_size]? [block_size, ..., 1, 0]?
-	 	float * y_book = &lms_state[n]; //Here we create a new pointer, which we call y_book, using the notation in the course book. Which index of lms_state should we use to set up y_book?
-	    float * x_grej = &xhat[n];
-		arm_dot_prod_f32(lms_coeffs, y_book, block_size, x_grej); 
-		          //Here '*' implies the dot product. Either use arm_dot_prod_f32 or use a loop to compute xhat[n]
-	    e[n] = x[n] - *x_grej;                		//e[n] is a scalar, so do we need to do any looping here?
-	 	int m;
+	 int n, m;
+	 for (n = 0; n<block_size; n++){		//Looping n over [0, 1, 2, ..., block_size]
+	 	float * y_book = &lms_state[n];		//New pointer y_book, using n as index
+	    float * x_hat_n = &xhat[n];			//New pointer x_hat_n, using n as index
+		arm_dot_prod_f32(lms_coeffs, y_book, lms_taps, x_hat_n); //Dot product using arm_dot_prod_f32
+	    e[n] = x[n] - *x_hat_n;				//e[n] is a scalar, so no looping needed
 		for (m = 0; m<lms_taps; m++){
-			lms_coeffs[m] += 2 * lms_mu * y_book[m] * e[n];
+			lms_coeffs[m] += 2 * lms_mu * y_book[m] * e[n]; // Using for loop to update the vector lms_coeffs with the vector y multiplied by scalars 2, mu, e[n].
 		}
-		//lms_coeffs += 2 * mu * y_book * e[n];      //Use some type of loop to update the vector lms_coeffs with the vector y multiplied by scalars 2, mu, e[n].
 	}
 	 /* to here */
 
